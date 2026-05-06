e1553993159927420b80a3195f42c997801846aa
ggpradeep <80970765+ggpradeep@users.noreply.github.com>
2026-05-05 19:19:00 -0700

    Add files via upload

---
+++ Notes.py
@@ -0,0 +1,8 @@
+Ammount = int(input("enter a number"))
+note1 = Ammount//100
+note2 = Ammount//50
+note3 = Ammount//10
+
+print("Notes of 100 dollars", note1, "notes")
+print("Notes of 50 dollars", note2, "notes")
+print("Notes of 10 dollars", note3, "notes")
